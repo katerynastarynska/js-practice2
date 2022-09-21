d55dc2aa9b7dd7d7455d1ca8f7aa2e8ff722cd2c
katerynastarynska <katerynastarynska@gmail.com>
2022-09-21 09:27:45 -0500

    js-practice2

---
+++ index.js
@@ -0,0 +1,11 @@
+for (let i = 0; i <= 100; i += 1) {
+    if (i % 2 === 0) {
+      console.log("foo");
+    } else if (i % 3 === 0) {
+      console.log("buzz");
+    } else if (i % 2 === 0 && i % 3 === 0) {
+      console.log("fooBuzz");
+    } else {
+      console.log(i);
+    }
+  }
